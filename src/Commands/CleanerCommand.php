7d5dc2e425706945b1daa536ab968e674f7583cb
Reza Amini <rezaaminiroyal@gmail.com>
2022-03-21 16:43:39 +0330

    Add alerty:clear command

---
+++ src/Commands/CleanerCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Alerty\Commands;
+
+use Illuminate\Console\Command;
+use Alerty\Models\QueryEntry;
+
+class CleanerCommand extends Command
+{
+    protected $signature = 'alerty:clear';
+    protected $description = 'Clean the alerts from database.';
+
+    public function handle()
+    {
+        $status = $this->confirm("Do you really want to clean the alerts?", true);
+
+        if (! $status){
+            $this->info("It was canceled!");
+            return;
+        }
+
+        QueryEntry::query()->delete();
+
+        $this->info("The alert table was cleaned, You are fresh now!");
+    }
+}
